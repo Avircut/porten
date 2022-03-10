b6e4113a1c66f39ae5299a0b9dc5a95a30007ed7
Avircut <lomoff.slaw@yandex.ru>
2022-03-10 15:12:40 +0700

    Burger animation bug fix

--- src/components/header/burger/burger.js
+++ src/components/header/burger/burger.js
@@ -1,10 +1,11 @@
 class Burger{
   constructor(el){
     this.el = el;
-    this.overlay = el.parentNode.querySelector('.overlay')
+    this.overlay = el.parentNode.querySelector('.overlay');
     this.isAnimating = false;
     this.body = document.querySelector('body');
     this.overlay.classList.add('disabled');
+    this.links = this.overlay.querySelector('.burger__links');
     this.isOpen = false;
     el.addEventListener('click', (e) => {
       this.onClick(e)
@@ -19,7 +20,8 @@ class Burger{
         this.isOpen=true;
         this.overlay.classList.remove('disabled');
         this.overlay.classList.remove('slideDown');
-        this.overlay.style.height=`${document.body.clientHeight}px`;
+        this.overlay.style.height=`${this.links.offsetHeight}px`;
+
       })
     } else if (!this.isAnimating) {
       this.overlay.classList.add('slideUp');
